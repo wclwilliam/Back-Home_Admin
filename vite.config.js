03e48a190c7b471efcfed95c1ce89831dfdcbdc3
Grim2485 <k29267597@gmail.com>
2026-01-17 12:01:32 +0800

    部屬路徑更改

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  }, 
-  base: '/admin/',  //預設url有admin前缀
+// 原本export default defineConfig({…})
+// 變成export default defineConfig(({mode})=>{return{...}})
 
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+export default defineConfig(({ mode }) => {
+  // 讀取 .env、.env.[mode]，第三個參數用 '' 才會包含非 VITE_ 前綴 
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      // vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
         @import "@/assets/scss/base/_color.scss";
         @import "@/assets/scss/base/_font.scss";
         @import "@/assets/scss/base/_var.scss";
         @import "@/assets/scss/mixin/_mixins.scss";`
+        }
       }
-    }
+    },
+    base: env.VITE_BASE || '/admin/', build: { outDir: env.VITE_OUT_DIR || 'dist', }
   }
-}) 
+})
