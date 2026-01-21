56b7878200a8efd7023765b9a974d13a57327464
shih3511 <sonw1018@gmail.com>
2026-01-21 23:57:18 +0800

    活動詳細資訊表單修改

--- src/components/activity/ActivityForm.vue
+++ src/components/activity/ActivityForm.vue
@@ -10,39 +10,33 @@ const props = defineProps({
   },
 })
 
-// 定義事件，當按下儲存時通知父層
 const emit = defineEmits(['save'])
 const router = useRouter()
 
-// 1. 控制編輯狀態
+// 控制編輯狀態
 const isEditing = ref(false)
 
-// 備份資料用（取消編輯時還原）
+// 備份資料用
 let backupData = {}
 onMounted(() => {
   if (props.formData.isNew) {
-    // 如果是新資料，自動開啟編輯模式
     isEditing.value = true
-    // 不需要備份，因為沒有舊資料可以還原
   }
 })
 
-// 開啟編輯模式
 const enableEdit = () => {
-  // 深層拷貝當前資料做備份
   const rawCopy = JSON.parse(JSON.stringify(props.formData))
   backupData = rawCopy
   isEditing.value = true
 }
 
-// 取消編輯
 const cancelEdit = () => {
   if (props.formData.isNew) {
-    router.go(-1) // 回上一頁
+    router.go(-1)
     return
   }
 
-  // 還原資料
+  // 還原資料邏輯
   if (backupData.activityTime && typeof backupData.activityTime[0] === 'string') {
     backupData.activityTime = [
       new Date(backupData.activityTime[0]),
@@ -55,17 +49,15 @@ const cancelEdit = () => {
       new Date(backupData.registrationTime[1]),
     ]
   }
-
+  //取消編輯
   Object.assign(props.formData, backupData)
   isEditing.value = false
   ElMessage.info('已取消編輯')
 }
-
-// 儲存編輯
+//儲存編輯
 const saveEdit = () => {
   isEditing.value = false
   emit('save', props.formData)
-
   ElMessage.success('儲存成功')
   if (props.formData.isNew) {
     props.formData.isNew = false
@@ -76,77 +68,82 @@ const saveEdit = () => {
 <template>
   <div class="form-container">
     <div class="section-header">
-      <h2 class="section-title">核心資訊</h2>
-      <template v-if="!isEditing">
-        <el-button type="primary" plain class="edit-btn" @click="enableEdit"> 編輯 </el-button>
-      </template>
-      <template v-else>
-        <el-button @click="cancelEdit">取消</el-button>
-        <el-button type="primary" @click="saveEdit">儲存</el-button>
-      </template>
+      <h2 class="section-title">活動詳細資訊</h2>
+      <div class="action-buttons">
+        <template v-if="!isEditing">
+          <el-button class="actionBtn" plain @click="enableEdit"> 編輯 </el-button>
+        </template>
+        <template v-else>
+          <el-button class="actionBtn" plain @click="cancelEdit">取消</el-button>
+          <el-button class="actionBtn" plain @click="saveEdit">儲存</el-button>
+        </template>
+      </div>
     </div>
 
-    <el-form :model="formData" label-width="100px" label-position="left">
+    <el-form :model="formData" label-width="100px" label-position="left" class="customForm">
       <el-row :gutter="40">
         <el-col :span="12">
-          <el-form-item label="活動編號 ">
-            <el-input v-model="formData.id" disabled />
+          <el-form-item label="活動編號">
+            <el-input v-model="formData.id" disabled class="readOnlyInput" />
           </el-form-item>
           <el-form-item label="活動類別">
-            <el-select v-model="formData.category" placeholder="請選擇類別" :disabled="!isEditing">
+            <el-select
+              v-model="formData.category"
+              placeholder="請選擇類別"
+              :disabled="!isEditing"
+              style="width: 100%"
+            >
               <el-option label="淨灘" value="淨灘" />
               <el-option label="巡守" value="巡守" />
               <el-option label="照護" value="照護" />
             </el-select>
           </el-form-item>
           <el-form-item label="活動區域">
-            <el-input v-model="formData.region" disabled />
+            <el-input v-model="formData.region" disabled class="readOnlyInput" />
           </el-form-item>
-          <el-form-item label="最大志工人類">
+          <el-form-item label="最大志工數">
             <el-input v-model="formData.maxVolunteers" :disabled="!isEditing" />
           </el-form-item>
-          <el-form-item label="目前報名志工人類">
-            <el-input v-model="formData.currentVolunteers" disabled />
+          <el-form-item label="目前報名數">
+            <el-input v-model="formData.currentVolunteers" disabled class="readOnlyInput" />
           </el-form-item>
         </el-col>
 
         <el-col :span="12">
-          <el-form-item label="發布管理者帳號" class="multi-line-label">
-            <el-input v-model="formData.publisher" disabled />
+          <el-form-item label="管理者帳號">
+            <el-input v-model="formData.publisher" disabled class="readOnlyInput" />
           </el-form-item>
           <el-form-item label="發布時間">
-            <el-input v-model="formData.publishTime" disabled />
+            <el-input v-model="formData.publishTime" disabled class="readOnlyInput" />
           </el-form-item>
           <el-form-item label="活動標題">
             <el-input v-model="formData.title" :disabled="!isEditing" />
           </el-form-item>
           <el-form-item label="活動狀態">
-            <div style="display: flex; align-items: center; gap: 10px; width: 100%">
-              <el-select
-                v-model="formData.status"
-                placeholder="請選擇狀態"
-                :disabled="!isEditing"
-                style="width: 100%"
-              >
-                <el-option label="草稿" value="草稿" />
-                <el-option :label="'發布 ( ' + formData.detailStatus + ' )'" value="發布">
-                  <span>發布</span>
-                  <span
-                    v-if="formData.status === '發布'"
-                    style="
-                      float: right;
-                      color: #4fa8c3;
-                      font-size: 13px;
-                      font-weight: bold;
-                      margin-left: 10px;
-                    "
-                  >
-                    ( {{ formData.detailStatus }} )
-                  </span>
-                </el-option>
-                <el-option label="取消" value="取消" />
-              </el-select>
-            </div>
+            <el-select
+              v-model="formData.status"
+              placeholder="請選擇狀態"
+              :disabled="!isEditing"
+              style="width: 100%"
+            >
+              <el-option label="草稿" value="草稿" />
+              <el-option :label="'發布 ( ' + formData.detailStatus + ' )'" value="發布">
+                <span>發布</span>
+                <span
+                  v-if="formData.status === '發布'"
+                  style="
+                    float: right;
+                    color: #4fa8c3;
+                    font-size: 13px;
+                    font-weight: bold;
+                    margin-left: 10px;
+                  "
+                >
+                  ( {{ formData.detailStatus }} )
+                </span>
+              </el-option>
+              <el-option label="取消" value="取消" />
+            </el-select>
           </el-form-item>
           <el-form-item label="活動地點">
             <el-input v-model="formData.location" :disabled="!isEditing" />
@@ -159,7 +156,7 @@ const saveEdit = () => {
           v-model="formData.activityTime"
           type="datetimerange"
           range-separator="---"
-          format="YYYY-MM-DD-HH:mm"
+          format="YYYY-MM-DD HH:mm"
           :disabled="!isEditing"
           style="width: 100%"
         />
@@ -170,13 +167,11 @@ const saveEdit = () => {
           v-model="formData.registrationTime"
           type="datetimerange"
           range-separator="---"
-          format="YYYY-MM-DD-HH:mm"
+          format="YYYY-MM-DD HH:mm"
           :disabled="!isEditing"
           style="width: 100%"
         />
       </el-form-item>
-
-      <h2 class="section-title" style="margin-top: 30px">詳細活動內容</h2>
 
       <el-form-item label="活動簡介">
         <el-input
@@ -199,109 +194,164 @@ const saveEdit = () => {
       </el-form-item>
 
       <el-form-item label="封面照片">
-        <div class="upload-area">
-          <el-upload action="#" :auto-upload="false" :show-file-list="false">
-            <el-button class="upload-btn">上傳檔案 +</el-button>
+        <div class="uploadSection">
+          <el-upload
+            class="uploadBtn"
+            action="#"
+            :auto-upload="false"
+            :show-file-list="false"
+            :disabled="!isEditing"
+          >
+            <el-button :disabled="!isEditing">上傳檔案 +</el-button>
           </el-upload>
-          <span>測試.png</span>
+
+          <div class="imagePreview" v-if="formData.imageUrl">
+            <el-image :src="formData.imageUrl" fit="cover" class="previewImg" />
+            <span class="fileName">{{ formData.imageName }}</span>
+          </div>
+          <div class="imagePreview" v-else>
+            <div class="image-placeholder"></div>
+            <span class="fileName">尚未上傳</span>
+          </div>
         </div>
-        <div class="image-placeholder"></div>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <style lang="scss" scoped>
-$disable-col: #dcdcdc;
-$highlight-color1: #4fa8c3;
-$btn-bg: #cccccc;
+$primary-color: #0e6273;
+$secondary-color: #2c6e88;
 $text-color: #333333;
-$box-shadow: #aaaaaa;
-$tab-col: #4fa8c3;
-$highlight-color2: #e14720;
+$disable-bg: #dcdcdc;
 
 .form-container {
   padding: 20px 0;
 }
+
 .section-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
-}
-.section-title {
-  color: $primary-color;
-  font-size: 18px;
-  font-weight: bold;
-  margin-bottom: 15px;
-}
-.edit-btn,
-.el-button {
-  border: 1px solid $secondary-color;
-  color: $secondary-color;
-  background: transparent;
-  &:hover {
-    background-color: $secondary-color;
-    color: $text-white;
-  }
-}
-.el-button + .el-button {
-  background-color: $secondary-color;
-  color: $text-white;
-  &:hover {
-    background-color: $highlight-color2;
-    border: 1px solid $highlight-color2;
-    color: $text-white;
-  }
-}
-.upload-area {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  width: 100%;
-}
-.upload-btn {
-  border: 1px solid $secondary-color;
-  color: $secondary-color;
-  background: transparent;
-  &:hover {
-    background-color: $secondary-color;
-    color: $text-white;
-  }
-}
-.image-placeholder {
-  width: 200px;
-  height: 150px;
-  background-color: $disable-col;
-  margin-top: 10px;
+  margin-bottom: 24px;
+  border-bottom: 2px solid $primary-color;
+  padding-bottom: 10px;
 }
 
-/* Form Styles */
-:deep(.el-form-item__label) {
+.section-title {
+  color: $primary-color;
+  font-size: 24px;
   font-weight: bold;
-  color: $text-color;
-  line-height: 1.4;
+  margin: 0;
+}
+//按鈕
+.actionBtn {
+  width: 100px;
+  border: 1px solid $secondary-color;
+  color: $secondary-color;
+  font-weight: bold;
+
+  &:hover {
+    background-color: $secondary-color;
+    color: $text-white;
+  }
+}
+
+.customForm {
+  :deep(.el-form-item__label) {
+    font-weight: bold;
+    color: $text-color;
+    letter-spacing: 1px;
+  }
+
+  .readOnlyInput {
+    :deep(.el-input__wrapper) {
+      background-color: $disable-bg;
+      box-shadow: none;
+      border: 1px solid #999;
+      border-radius: 4px;
+    }
+
+    :deep(.el-input__inner) {
+      color: $text-color;
+      font-weight: bold;
+      text-align: center;
+      -webkit-text-fill-color: $text-color;
+    }
+  }
+
+  // 一般輸入框樣式
+  :deep(.el-input__wrapper),
+  :deep(.el-textarea__inner),
+  :deep(.el-select__wrapper) {
+    border-radius: 4px;
+    box-shadow: 0 0 0 1px $secondary-color inset;
+
+    &.is-focus {
+      box-shadow: 0 0 0 1px $primary-color inset !important;
+    }
+  }
+
+  // disabled 樣式
+  :deep(.el-input.is-disabled:not(.readOnlyInput) .el-input__wrapper),
+  :deep(.el-textarea.is-disabled .el-textarea__inner),
+  :deep(.el-select__wrapper.is-disabled) {
+    background-color: $text-white;
+    box-shadow: 0 0 0 1px #e4e7ed inset;
+    color: $text-color;
+  }
+}
+//圖片上傳
+.uploadSection {
   display: flex;
-  align-items: center;
-}
-:deep(.el-input.is-disabled .el-input__inner) {
-  -webkit-text-fill-color: $text-color;
-}
-:deep(.el-select__wrapper.is-disabled .el-select__selected-item) {
-  color: $text-color !important;
-}
-.multi-line-label :deep(.el-form-item__label) {
-  white-space: pre-line;
-}
-:deep(.el-input__wrapper),
-:deep(.el-textarea__inner) {
-  border-radius: 0;
-  box-shadow: 0 0 0 1px $box-shadow inset;
-}
-:deep(.is-disabled .el-input__wrapper),
-:deep(.is-disabled .el-textarea__inner) {
-  background-color: $disable-col;
-  box-shadow: 0 0 0 1px $box-shadow inset;
-  color: $text-color;
+  align-items: flex-start;
+  gap: 20px;
+  width: 100%;
+
+  .uploadBtn {
+    :deep(.el-button) {
+      border: 1px solid $secondary-color;
+      color: $secondary-color;
+      width: 120px;
+
+      &:hover {
+        background-color: $secondary-color;
+        color: $text-white;
+      }
+      &.is-disabled {
+        background-color: $text-white;
+        border-color: #e4e7ed;
+        color: #a8abb2;
+      }
+    }
+  }
+  //圖片預覽
+  .imagePreview {
+    display: flex;
+    align-items: flex-end;
+    gap: 20px;
+
+    .previewImg {
+      width: 200px;
+      height: 120px;
+      border-radius: 4px;
+      margin-bottom: 4px;
+      border: 1px solid #ccc;
+    }
+
+    .image-placeholder {
+      width: 200px;
+      height: 120px;
+      background-color: $disable-bg;
+      border: 1px dashed #999;
+      margin-bottom: 4px;
+    }
+
+    .fileName {
+      font-size: 12px;
+      color: $text-color;
+      margin-bottom: 5px; // 對齊底部
+    }
+  }
 }
 </style>
