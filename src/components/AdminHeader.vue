ef4d9482ae74d3d64f7b889f7bcec38e7104cc66
Grim2485 <k29267597@gmail.com>
2026-01-21 10:36:24 +0800

    header組件

---
+++ src/components/AdminHeader.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+const props = defineProps({
+  showLoginoutBtn: {  //如果不要登出按鈕在自己頁面設  :showLoginoutBtn=false
+    type: Boolean,
+    default: true
+  },
+  title: {
+    type: String,
+    default: '管理頁面'
+  }
+});
+</script>
+<template>
+    <div class="headerSection">
+            <h2 class="pageTitle">{{props.title}}</h2>
+            <div class="userInfo" v-if="props.showLoginoutBtn">
+                <span>管理者帳號</span>
+                <el-button class="logoutBtn" size="small" plain>登出</el-button>
+            </div>
+        </div>
+</template>
+<style scoped lang="scss">
+    .headerSection {
+  border-bottom: 2px solid $primary-color;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+  padding-bottom: 24px;
+
+  .pageTitle { 
+    font-size: 36px;
+    color: $primary-color;
+    font-weight: bold;
+    margin: 0;
+  }
+
+  .userInfo {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    font-size: 14px;
+    color: $text-color;
+    .logoutBtn {
+    border: 1px solid $secondary-color;
+    color: $secondary-color;
+    }
+  }
+}
+</style>
--- src/layouts/AdminHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="news-view container">
-    <el-row>
-      <h1>最新消息管理</h1>
-    </el-row>
-  </div>
-</template>
