62c33d050ec69b4d7035926a1599e6412a79db62
baekyeeee <baekyeeee@gmail.com>
2026-02-06 13:12:07 +0800

    消息圖片上傳路徑修改

--- src/views/News/NewsAdd.vue
+++ src/views/News/NewsAdd.vue
@@ -83,7 +83,6 @@ class MyUploadAdapter {
           if (result.error) {
             reject(result.error.message);
           } else {
-            // 取得 env 裡的檔案基本路徑：http://localhost:8888/api/ 或 https://tibamef2e.com/.../api/
             const fileBaseUrl = import.meta.env.VITE_FILE_URL;
 
             resolve({
--- src/views/News/NewsEdit.vue
+++ src/views/News/NewsEdit.vue
@@ -36,11 +36,10 @@ class MyUploadAdapter {
           if (result.error) {
             reject(result.error.message);
           } else {
-            // 取得 env 裡的檔案基本路徑：http://localhost:8888/api/ 或 https://tibamef2e.com/.../api/
-            const fileBaseUrl = import.meta.env.VITE_FILE_URL;
+            const fileBaseUrl = import.meta.env.VITE_FILE_URL; // 這裡已有 uploads/
 
             resolve({
-              // 動態拼接：Base URL + PHP 回傳的相對路徑
+              // 拼接後會變成：.../api/uploads/news/filename.jpg
               default: fileBaseUrl + result.url
             });
           }
